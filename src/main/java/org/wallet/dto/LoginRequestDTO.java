b3f6a92db091ab664e18a8c47fca4e8b6c59276a
Guna <gunasekaranb2808@gmail.com>
2026-01-27 11:01:46 +0530

    Update LoginRequestDTO.java

--- src/main/java/org/wallet/dto/LoginRequestDTO.java
+++ src/main/java/org/wallet/dto/LoginRequestDTO.java
@@ -5,7 +5,7 @@ public class LoginRequestDTO {
     private String username;
     private String password;
 
-    public getUsername() {
+    public String getUsername() {
         return username;
     }
 
